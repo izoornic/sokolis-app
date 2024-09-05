fdcf6b4b754d8d5c9e26ede3a9c6b7663356390d
izoornic <izornic@gmail.com>
2024-09-05 18:19:14 +0200

    he

--- public/index.php
+++ public/index.php
@@ -1,6 +1,7 @@
 <?php
 
-use Illuminate\Http\Request;
+echo "Helooo";
+/* use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
@@ -14,4 +15,4 @@ require __DIR__.'/../vendor/autoload.php';
 
 // Bootstrap Laravel and handle the request...
 (require_once __DIR__.'/../bootstrap/app.php')
-    ->handleRequest(Request::capture());
+    ->handleRequest(Request::capture()); */
